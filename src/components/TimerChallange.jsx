8334f335995784a7e939b4e5d0dd62ddc1464209
Dmytro Lukashenko <d.a.lukashenko@gmail.com>
2024-09-08 17:17:33 +0300

    use useRef for creating a timer

--- src/components/TimerChallange.jsx
+++ src/components/TimerChallange.jsx
@@ -1,16 +1,34 @@
+import { useState, useRef } from "react";
+
 const TimerChallange = ({ title, targetTime }) => {
+    const timer = useRef();
+
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
+    
+    const handleStart = () => {
+        timer.current = setTimeout(() => {
+            setTimerExpired(true);
+        }, targetTime * 1000);
+        setTimerStarted(true);
+    }
+    const handleStop = () => {
+        clearTimeout(timer.current);
+        setTimerStarted(false);
+    }
     return <section className="challenge">
         <h2>{title}</h2>
+        {timerExpired && <p>You lost!</p>}
         <p className="challenge-time">
             {targetTime} seconds{targetTime > 1 ? 's' : ''}
         </p>
         <p>
-            <button>
-                Start Challange
+            <button onClick={timerStarted ? handleStop : handleStart}>
+                {timerStarted ? 'Stop' : 'Start'} Challange
             </button>
         </p>
-        <p className="">
-            Time is running
+        <p className={timerStarted ? 'active' : undefined}>
+            {timerStarted ? 'Time is running...' : 'Timer inactive'}
         </p>
     </section>
 }
