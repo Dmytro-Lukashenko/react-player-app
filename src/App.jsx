66519958c71b8f7c1103dd0f735fb063472d7575
Dmytro Lukashenko <d.a.lukashenko@gmail.com>
2024-09-08 17:48:37 +0300

    feat: add dialog html element to the ResultModal component and use forwardRef to use it in the TimerChallenge conponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import Player from './components/Player.jsx';
-import TimerChallange from './components/TimerChallange.jsx'
+import TimerChallenge from './components/TimerChallenge.jsx'
 
 function App() {
   return (
     <>
       <Player />
       <div id="challenges">
-        <TimerChallange title="Easy" targetTime={1} />
-        <TimerChallange title="Not Easy" targetTime={5} />
-        <TimerChallange title="Getting tough" targetTime={10} />
-        <TimerChallange title="Pros only" targetTime={15} />
+        <TimerChallenge title="Easy" targetTime={1} />
+        <TimerChallenge title="Not Easy" targetTime={5} />
+        <TimerChallenge title="Getting tough" targetTime={10} />
+        <TimerChallenge title="Pros only" targetTime={15} />
       </div>
     </>
   );
---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,46 @@
+import { useState, useRef } from "react";
+import ResultModal from "./ResultModal";
+
+const TimerChallange = ({ title, targetTime }) => {
+    const timer = useRef();
+    const dialog = useRef();
+
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
+
+    const handleStart = () => {
+        timer.current = setTimeout(() => {
+            setTimerExpired(true);
+            dialog.current.showModal();
+        }, targetTime * 1000);
+        setTimerStarted(true);
+    }
+    const handleStop = () => {
+        clearTimeout(timer.current);
+        setTimerStarted(false);
+    }
+    return (
+        <>
+            <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+            <section className="challenge">
+                <h2>{title}</h2>
+                {timerExpired && <p>You lost!</p>}
+                <p className="challenge-time">
+                    {targetTime} seconds{targetTime > 1 ? 's' : ''}
+                </p>
+                <p>
+                    <button onClick={timerStarted ? handleStop : handleStart}>
+                        {timerStarted ? 'Stop' : 'Start'} Challange
+                    </button>
+                </p>
+                <p className={timerStarted ? 'active' : undefined}>
+                    {timerStarted ? 'Time is running...' : 'Timer inactive'}
+                </p>
+            </section>
+        </>
+    )
+
+
+}
+
+export default TimerChallange;
